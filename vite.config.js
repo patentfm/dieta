1f1b1103e6924b394e620c8716a8882f5152bf6a
patentfm <wrzosdev@gmail.com>
2025-11-26 20:04:46 +0100

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // Zmień 'dieta' na nazwę Twojego repozytorium GitHub
+  base: '/dieta/',
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist'
   }
 })
-
